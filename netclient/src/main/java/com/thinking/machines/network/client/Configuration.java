da3f0a9106e7a3df9b7661fbdcdedb862638a89e
Sameer <sameerjhawar28@gmail.com>
2022-09-14 16:01:47 +0530

    init project

---
+++ netclient/src/main/java/com/thinking/machines/network/client/Configuration.java
@@ -0,0 +1,50 @@
+package com.thinking.machines.network.client;
+import org.xml.sax.*;
+import javax.xml.xpath.*;
+import java.io.*;
+import com.thinking.machines.network.common.exceptions.*;
+class Configuration
+{
+private static String host="";
+private static int port=-1;
+private static boolean fileMissing=false;
+private static boolean malformed=false;
+static 
+{
+try
+{
+File file=new File("server.xml");
+if(file.exists())
+{
+InputSource inputSource=new InputSource("server.xml");
+XPath xpath=XPathFactory.newInstance().newXPath();
+String host=xpath.evaluate("//server/@host",inputSource);
+String port=xpath.evaluate("//server/@port",inputSource);
+Configuration.port=Integer.parseInt(port);
+Configuration.host=host;
+}
+else
+{
+fileMissing=true;
+}
+}catch(Exception e)
+{
+malformed=true;
+// do nothing
+}
+}
+public static String getHost() throws NetworkException
+{
+if(fileMissing) throw new NetworkException("server.xml is missing,read documentation");
+if(malformed) throw new NetworkException("server.xml not configured according to documentation");
+if(host=="" || host.trim().length()==0) throw new NetworkException("server.xml not configured according to documentation");
+return host;
+}
+public static int getPort() throws NetworkException
+{
+if(fileMissing) throw new NetworkException("server.xml is missing,read documentation");
+if(malformed) throw new NetworkException("server.xml not configured according to documentation");
+if(port<0 || port>49151) throw new NetworkException("server.xml contains invalid port number,read documentation");
+return port;
+}
+}
